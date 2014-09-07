8432d813e1bfbc1c779b02e80b6d880610ac95ed
Benjamin Inada <benjamin.inada@gmail.com>
2014-09-06 22:00:45 -0700

    Modify Gruntfile to remove need for app/ directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,37 +4,36 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       dist: {
-        src: ['app/js/**/*.js'],
-        dest: 'js/main.js'
+        src: ['js/**/*.js'],
+        dest: 'dist/js/compiled.js'
       }
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
         mangle: true
       },
       dist: {
         files: {
-          'js/main.min.js': ['js/main.js']
+          'js/compiled.min.js': ['dist/js/compiled.js']
         }
       }
     },
     cssmin: {
       minify: {
         expand: true,
-        cwd: 'app/css/',
+        cwd: 'css/',
         src: ['*.css', '!*.min.css'],
-        dest: 'css/',
+        dest: 'dist/css/',
         ext: '.min.css'
       },
       combine: {
         files: {
-          'css/main.min.css': ['css/*.min.css']
+          'css/compiled.min.css': ['dist/css/*.min.css']
         }
       }
     },
     jshint: {
-      files: ['Gruntfile.js', 'app/js/**/*.js'],
+      files: ['Gruntfile.js', 'js/**/*.js'],
       options: {
         globals: {
           jQuery: true,
@@ -45,43 +44,36 @@ module.exports = function(grunt) {
         }
       }
     },
-    copy: {
-      main: {
-        files:[
-          {
-            expand: true,
-            src: '**',
-            cwd: 'app/',
-            dest: '.'
-          },
-        ]
+    clean: ['dist', 'css/*.min.css', 'js/*.min.js'],
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: '.'
+        }
       }
-    },
-    clean: {
-      dev: ["index.html", "css/", "js/"],
-      deliver: ["node_modules/"]
     },
     watch: {
       dev: {
-        files: ['app/index.html', 'app/css/**/*', 'app/js/**/*', '!bower_components/**'],
-        tasks: ['default']
+        files: ['index.html', 'css/**/*', 'js/**/*', '!bower_components/**'],
+        tasks: ['latch']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('lint',    ['jshint']);
   grunt.registerTask('minify',  ['concat', 'cssmin', 'uglify']);
-  grunt.registerTask('build', ['clean:dev', 'lint', 'minify', 'copy']);
+  grunt.registerTask('build', ['clean', 'lint', 'minify']);
   
-  grunt.registerTask('default', ['build', 'watch']);
-  grunt.registerTask('deliver', ['build', 'clean:deliver']);
+  grunt.registerTask('default', ['build', 'connect', 'watch']);
+  grunt.registerTask('latch', ['build', 'watch']);
 
 };
