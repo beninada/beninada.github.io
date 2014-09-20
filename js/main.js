4520454abc28e79daa3e9dda1eab39529d5f5b2d
Benjamin Inada <benjamin.inada@gmail.com>
2014-09-20 16:46:05 -0700

    Implement thanks on .btn-send click.

--- js/main.js
+++ js/main.js
@@ -19,14 +19,20 @@ var $span = $('span', $swap).hide();
   c = ++c % nouns.length;
 }());
 
-function postContactToGoogle() {
+function postContact() {
   var email = $('#email').val();
   var message = $('#message').val();
 
-  $.ajax({
-    url: "https://docs.google.com/forms/d/1IWlkSg1Utl4ZiHcdjIwwn6ZFU8GCDMyeifUfAM1l0z8/formResponse",
-    data: { "entry.132395288": email, "entry.1268391083": message },
-    type: "POST",
-    dataType: "xml"
-  });
+  // $.ajax({
+  //   url: "https://docs.google.com/forms/d/1IWlkSg1Utl4ZiHcdjIwwn6ZFU8GCDMyeifUfAM1l0z8/formResponse",
+  //   data: { "entry.132395288": email, "entry.1268391083": message },
+  //   type: "POST",
+  //   dataType: "xml"
+  // });
 }
+
+$('.btn-send').click(function () {
+  $('.contact-me .form-group').addClass('posted');
+  $(this).closest('.form-group').find('input[type=email], textarea, button').attr('disabled', true);
+  $(this).closest('.form-group').find('input[type=email], textarea').val('');
+});
